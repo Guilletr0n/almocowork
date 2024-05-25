0cbc0dfa2b47c44903060dabf58297d00bd27c8e
Guilletr0n <guillermo.gonzalez@moven.pro>
2024-05-25 10:51:37 +0200

    Open graph config

--- coworkingalmonaster.site/astro.config.mjs
+++ coworkingalmonaster.site/astro.config.mjs
@@ -5,6 +5,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://coworkingalmonaster.site/",
   integrations: [mdx(), sitemap(), preact()],
 });
