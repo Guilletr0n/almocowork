2cceb8671bf64481814467db3fbb0d07c3b703f3
gonzalezdecastro.guillermo <gonzalezdecastro.guillermo@gmail.com>
2024-07-26 19:32:11 +0000

    Install Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://coworkingalmonaster.site/",
   integrations: [mdx(), sitemap(), preact()],
+  output: "server",
+  adapter: vercel()
 });
